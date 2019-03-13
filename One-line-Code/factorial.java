e17404c1c35b648f4a3d988206acabacc4e61918
SquirrelChen <827321394@qq.com>
2019-03-13 15:08:56 +0800

    The factorial.java is use one-line-code to describes the factorial alorihtm of N and the return the result.

---
+++ One-line-Code/factorial.java
@@ -0,0 +1,13 @@
+public class factorial{
+
+	public static int factorial(int n)
+	{
+		return (n>0)? n*factorial(n-1):1;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("The result is "+factorial(10));
+	}
+
+}
