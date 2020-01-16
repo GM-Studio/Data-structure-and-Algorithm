feb83ac91da3bf0e6791b60dfed7222f23388954
SquirrelChen <13654811+GM-Studio@users.noreply.github.com>
2020-01-16 17:28:11 +0800

    update stringfilp.java
    Add manystring inversion function

--- String/StringFilp.java
+++ String/StringFilp.java
@@ -1,7 +1,7 @@
 package datastructure.string.stringFilp;
 
 /**
- *   字符串翻转
+ * 字符串翻转
  *
  * @author Squirrel-Chen
  * @DateTime 2020/1/16
@@ -9,31 +9,47 @@ package datastructure.string.stringFilp;
 
 public class StringFilp {
 
-     /**
-     *   目标字符串数组，转换后的字符串存储数组
+    /*
+     *  单个字符串的翻转
      */
-    private static char[] descString=new char[1024];
 
-    public static char[] stringFilp(String str)
-    {
-        descString=str.toCharArray();
-        int high,low,mid=0;
-        low=0;
-        high=descString.length-1;
-        mid=descString.length/2;
+    /**
+     * 目标字符串数组，转换后的字符串存储数组
+     */
+    private static char[] descString = new char[1024];
+
+    public static char[] stringFilp(String str) {
+        descString = str.toCharArray();
+        int high, low, mid = 0;
+        low = 0;
+        high = descString.length - 1;
+        mid = descString.length / 2;
         char swap;
-        for(;low<=mid&&high>=mid;low++,high--)
-        {
-            swap=descString[high];
-            descString[high]=descString[low];
-            descString[low]=swap;
+        for (; low <= mid && high >= mid; low++, high--) {
+            swap = descString[high];
+            descString[high] = descString[low];
+            descString[low] = swap;
         }
         return descString;
     }
 
-    public static void main(String[] args)
-    {
+    /*
+     *  多个字符串的翻转
+     */
+
+    public static String inversion(String str) {
+        String result = new String();
+        String[] strs = str.split(" ");
+        for (int i = strs.length - 1; i >= 0; i--) {
+            result += strs[i] + " ";
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
         System.out.println(stringFilp("algorithm"));
+        System.out.println(inversion("data and algorithm"));
     }
 
 }
